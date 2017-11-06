20b0f890e1cdca18869b881f506248fbf1489204
menase12 <menase.yonatan@gmail.com>
2017-11-06 16:32:10 -0500

    Initial commit

---
+++ src/main/java/com/example/helloworldwithvariable/HelloworldwithvariableApplication.java
@@ -0,0 +1,12 @@
+package com.example.helloworldwithvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldwithvariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldwithvariableApplication.class, args);
+	}
+}
